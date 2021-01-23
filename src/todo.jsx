c03c16e1800255eb38b858186ced3fe08c8736d3
Anishasharma023 <anishasharma023@gmail.com>
2021-01-23 20:52:48 +0530

    todo  list project

---
+++ src/todo.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import React, {Component} from 'react';
+
+class Todo extends Component{
+    render(){
+        return(
+            {
+            <Div></Div>
+            }
+        )
+    }
+}
